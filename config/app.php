acdc04ce19ffba6d571c0ee26cc35cb930f8bc96
Claudio Vieira <claudiovieira@Mac.lan>
2026-04-30 21:21:42 +0100

    feat: add config/app.php with supported langs and pages

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+define('SUPPORTED_LANGS',  ['en', 'pt', 'fr']);
+define('DEFAULT_LANG',     'en');
+define('SUPPORTED_PAGES',  ['home', 'privacy', 'terms', 'cookies']);
+define('SITE_URL',         'https://atlanticrowngroup.com');
+
+$OG_LOCALE = [
+  'en' => 'en_US',
+  'pt' => 'pt_PT',
+  'fr' => 'fr_FR',
+];
